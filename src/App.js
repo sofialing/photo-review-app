3b9d8defc9ecc1b3b606631c836293d2cdce1f4c
Sofia Ling <sofialing@live.se>
2021-01-05 09:21:22 +0100

    Implemented React Router and added Firebase config.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import './assets/scss/app.scss'
+
 const App = () => {
 	return (
-		<div className="App">
-			<h1>Photo Review App</h1>
-		</div>
+		<Router>
+			<Navbar />
+			<main className="container">
+				<Routes>
+					<Route path="/">
+						<h1>Photo Review App</h1>
+					</Route>
+					<Route path="login">
+						<h1>Login</h1>
+					</Route>
+					<Route path="signup">
+						<h1>Signup</h1>
+					</Route>
+					<Route path="forgot-password">
+						<h1>Forgot Password</h1>
+					</Route>
+					<Route path="albums">
+						<h1>Albums</h1>
+					</Route>
+				</Routes>
+			</main>
+		</Router>
 	);
 }
 
---
+++ src/firebase/index.js
@@ -0,0 +1,27 @@
+/**
+ * Initialize Firebase and Firestore
+ */
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firebase-firestore'
+import 'firebase/storage'
+
+// Firebase configuration from environment
+const firebaseConfig = {
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID
+}
+
+// initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+// get Firebase instances
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+export { auth, db, storage, firebase as default }
